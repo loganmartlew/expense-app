79c31f534ed66fd281145f3f555f1e50f05f2c2d
Logan Martlew <logan.martlew@gmail.com>
2022-05-13 15:56:49 +1200

    Display households on dashboard

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,19 +1,35 @@
-import { Link } from '@remix-run/react';
+import { json } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
 import authenticator from '~/services/auth.server';
 import { Container, Group, Title, Button } from '@mantine/core';
 import { Plus } from 'tabler-icons-react';
+import HouseholdService from '~/services/HouseholdService.server';
+import type { Household } from '@prisma/client';
 import type { LoaderFunction } from '@remix-run/node';
 import type { FC } from 'react';
 
+interface LoaderData {
+  households: Household[];
+}
+
 export let loader: LoaderFunction = async ({ request }) => {
-  return await authenticator.isAuthenticated(request, {
+  const user = await authenticator.isAuthenticated(request, {
     failureRedirect: '/login',
   });
+
+  if (!user) return user;
+  if (user instanceof Error) return user;
+
+  const households = await HouseholdService.getHouseholdsOfUser(user.id);
+  const data: LoaderData = { households };
+  return json(data);
 };
 
 interface Props {}
 
-const dashboard: FC<Props> = () => {
+const DashboardPage: FC<Props> = () => {
+  const { households } = useLoaderData<LoaderData>();
+
   return (
     <Container>
       <Group>
@@ -25,8 +41,13 @@ const dashboard: FC<Props> = () => {
           <Button leftIcon={<Plus size={18} />}>logout</Button>
         </Link>
       </Group>
+      {households.length < 1 ? (
+        <p>No Households</p>
+      ) : (
+        households.map(household => <p key={household.id}>{household.name}</p>)
+      )}
     </Container>
   );
 };
 
-export default dashboard;
+export default DashboardPage;
