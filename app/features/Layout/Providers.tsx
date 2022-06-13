81601eda8a6901beb14ea89cefb914036651e807
Logan Martlew <logan.martlew@gmail.com>
2022-06-13 19:39:49 +1200

    Added global styles

--- app/features/Layout/Providers.tsx
+++ app/features/Layout/Providers.tsx
@@ -1,5 +1,6 @@
-import { MantineProvider } from '@mantine/core';
+import { Global, MantineProvider } from '@mantine/core';
 import theme from '~/features/Style/theme';
+import globals from '~/features/Style/globals';
 import type { FC } from 'react';
 
 interface Props {}
@@ -7,6 +8,7 @@ interface Props {}
 const Providers: FC<Props> = ({ children }) => {
   return (
     <MantineProvider theme={theme} withNormalizeCSS withGlobalStyles>
+      <Global styles={globals} />
       {children}
     </MantineProvider>
   );
---
+++ app/features/Style/globals.ts
@@ -0,0 +1,8 @@
+import type { MantineTheme } from '@mantine/core';
+
+export default (theme: MantineTheme) => ({
+  a: {
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+});
